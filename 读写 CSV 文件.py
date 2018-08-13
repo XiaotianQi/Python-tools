20fbd02e523fe61926c1110d9f809941a03f2525
XiaotianQi <bnwseven@gmail.com>
2018-08-13 11:35:45 +0800

    补充：获取 dialect 列表及其内容

--- 读写 CSV 文件.py
+++ 读写 CSV 文件.py
@@ -9,13 +9,47 @@ def writeCsvFile(path, data, header=None, mode='w'):
             if header is not None:
                 writer.writerow(header)
 
-            for row in data:
-                writer.writerow(row)
+            writer.writerows(data)
 
             print("Successful! Path: %s." % path)
     except Exception as e:
         print("Error! Path: %s, Case: %s" % (path, e))
 
+def openCsvFile(path):
+    with open(path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+
+
+TEMPLATE = '''
+Dialect: "{name}"
+
+    delimiter   = {dl!r:<6}    skipinitialspace = {si!r}
+    doublequote = {dq!r:<6}    quoting          = {qu}
+    quotechar   = {qc!r:<6}    lineterminator   = {lt!r}
+    escapechar  = {ec!r:<6}
+'''
+quoting_modes = {
+    getattr(csv, n): n
+    for n in dir(csv)
+    if n.startswith('QUOTE_')
+}
+
+def showDialectList():
+    for name in sorted(csv.list_dialects()):
+        dialect = csv.get_dialect(name)
+
+        print(TEMPLATE.format(
+            name=name,
+            dl=dialect.delimiter,
+            si=dialect.skipinitialspace,
+            dq=dialect.doublequote,
+            qu=quoting_modes[dialect.quoting],
+            qc=dialect.quotechar,
+            lt=dialect.lineterminator,
+            ec=dialect.escapechar,
+        ))
 
 if __name__ == '__main__':
     header = ['Symbol','Price','Date','Time','Change','Volume']
@@ -28,3 +62,5 @@ if __name__ == '__main__':
         ["CAT", 78.29, "6/11/2007", "9:36am", -0.23,225400]
     ]
     writeCsvFile('testCSV.csv', data, header)
+    openCsvFile('testCSV.csv')
+    showDialectList()
