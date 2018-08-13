645f3379219e3acfdb6876d68dfd299ed1e4a546
XiaotianQi <bnwseven@gmail.com>
2018-08-13 20:45:44 +0800

    PDF 解析

---
+++ PDF 解析.py
@@ -0,0 +1,72 @@
+# _*_coding:utf-8_*_
+import os
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal, LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+
+'''
+解析单个 pdf 中的文本内容，保存到 txt 文件中
+'''
+
+def pdfParse(pdfPath, outPath='outfile.txt', password=''):
+    fp = open(pdfPath, 'rb')
+    # 创建 pdf 文档分析器
+    praser = PDFParser(fp)
+    # 创建 PDF文档
+    doc = PDFDocument()
+    # 连接 分析器与文档对象
+    praser.set_document(doc)
+    doc.set_parser(praser)
+    # 验证密码，如果没有密码，就使用空字符串
+    doc.initialize(password)
+
+    # 检测文档是否提供 txt 转换，不提供就忽略
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    else:
+        # 创建 PDF 资源管理器
+        rsrcmgr = PDFResourceManager()
+        # 创建 参数分析器
+        laparams = LAParams()
+        # 创建 创建聚合器
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        # 创建 PDF 解释器
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # 循环遍历列表，使用 .get_pages() 方法获取 page 列表
+        for page in doc.get_pages():
+            # 利用解释器的 process_page()方法解析读取单独页数
+            interpreter.process_page(page)
+            # layout 是一个 LTPage 对象，存放着 page 解析出的各种对象
+            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal等等,
+            # 想要获取文本就获得对象的text属性，
+            # 使用聚合器get_result()方法获取页面内容
+            layout = device.get_result()
+            for x in layout:
+                if (isinstance(x, LTTextBoxHorizontal)):
+                    with open(outPath, 'a') as f:
+                        results = x.get_text()
+                        f.write(results + '\n')
+
+'''
+解析多个 pdf 中的文本内容，保存到txt文件中
+'''
+
+def pdfFolder(folder, password=''):
+    # 获取指定目录下面的所有文件
+    files = os.listdir(folder)
+    # 获取pdf类型的文件放到一个列表里面
+    pdfFiles = [f for f in files if f.endswith(".pdf")]
+    for pdfFile in pdfFiles:
+        pdfPath = os.path.join(folder, pdfFile)
+        # 输出路径和文件格式
+        outPath = pdfFile.split(r'.')[0] + '.txt'
+        pdfParse(pdfPath, outPath, password)
+        print(outPath)
+
+
+if __name__ == '__main__':
+    pdfFolder('test')
+    print('Well Done!')
